6166ce61ad4fd39573f1b2f00e09315c214bd281
Felipe <philbahia@hotmail.com>
2023-01-05 11:27:54 -0300

    inicio angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { AboutComponent } from './componentes/about/about.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ExperienciaComponent,
+    AboutComponent,
+    EducacionComponent,
+    HabilidadesComponent
   ],
   imports: [
     BrowserModule,
